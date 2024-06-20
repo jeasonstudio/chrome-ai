68b517cb343fb5b7db7e3f53014d69558f0541fe
Jeason <jitong.zjt@alibaba-inc.com>
2024-06-20 11:35:36 +0800

    chore: add github actions

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
+  },
+});
